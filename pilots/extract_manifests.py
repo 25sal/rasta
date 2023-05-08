567096963b65215798d90132941b193d8bb1b5f7
salvatore <salvatore.venticinque@unicampania.it>
2023-05-08 11:26:34 +0200

    manifests compliant with IIIF v3

--- pilots/extract_manifests.py
+++ pilots/extract_manifests.py
@@ -4,14 +4,14 @@ import glob
 import os
 import subprocess
 pilot_dirs = {"norba": "data/pilots2/norba", "tifantina":"data/pilots2/tifantina"}
-
+base_dir="/home/salvatore/development/PycharmProjects/rasta/"
 for key in pilot_dirs.keys():
     m_dirs = glob.glob(pilot_dirs[key]+"/*")
     for m_dir in m_dirs:
         if os.path.isdir(m_dir):
-            print("node scripts/prova.js "+m_dir)
+            print("node scripts/prova.js "+base_dir+m_dir)
 
-            subprocess.run(["node", "scripts/prova.js", m_dir], cwd="/data/persone/alba/biiif")
+            subprocess.run(["node", "scripts/prova.js", base_dir+m_dir], cwd="/data/persone/alba/biiif")
             # exit(0)
             manifest_file = m_dir+"/index.json"
             path_ele = manifest_file.split("/")
@@ -19,6 +19,8 @@ for key in pilot_dirs.keys():
             new_file = new_file.replace(" ","_")
             new_file = new_file.lower()
             print(manifest_file, new_file)
-            shutil.copy(manifest_file, "data/pilots/"+key+"/manifests/"+new_file+".json")
+            if not os.path.isdir("data/pilots2/"+key+"/manifests/"):
+                os.makedirs("data/pilots2/"+key+"/manifests")
+            shutil.copy(manifest_file, "data/pilots2/"+key+"/manifests/"+new_file+".json")
 
 
--- pilots/modmanifest.py
+++ pilots/modmanifest.py
@@ -1,10 +1,42 @@
 import glob
 import json
 import os
+import logging
 
-manifest_dir ="data/pilots/tifantina/manifests"
-manifest_dir ="data/pilots/norba/manifests"
+
+logger = logging.getLogger()
+logging.basicConfig()
+logger.setLevel(logging.DEBUG)
+
+pilot = "tifantina"
+manifest_dir ="data/pilots/"+pilot+"/manifests"
 manifests = glob.glob(manifest_dir+"/*.json")
+
+
+def replace_items(manifest, jManifest, item_indexes):
+    print(manifest)
+
+    fid = open("data/pilots2/"+pilot+"/manifests/"+os.path.basename(manifest), "r")
+    filedata = fid.read()
+    new_data = filedata.replace("@none", "italian")
+    fid.close()
+    jbiif = json.loads(new_data)
+    for i in item_indexes:
+
+        print("------------------------------------------------------------")
+        print(jManifest['items'][i]['items'][0]['items'])
+        print("------------------------------------------------------------")
+        print(jbiif['items'][i]['items'][0]['items'])
+        print("------------------------------------------------------------")
+        if len(jbiif['items'][i]['items'][0]['items']) > 0:
+            jManifest['items'][i]['items'][0]['items'].append(jbiif['items'][i]['items'][0]['items'][0])
+            jManifest['items'][i]['width'] = jbiif['items'][i]['items'][0]['items'][0]['body']['width']
+            jManifest['items'][i]['height'] = jbiif['items'][i]['items'][0]['items'][0]['body']['height']
+        else:
+            logger.debug(manifest + " " + str(i))
+
+
+
 for manifest in manifests:
     if manifest.endswith("temp.json"):
         continue
@@ -32,13 +64,20 @@ for manifest in manifests:
             if "duration" not in  jManifest['items'][i].keys():
                 toberemoved.append(i)
     print(toberemoved)
-    removed = 0
+    replace_items(manifest, jManifest, toberemoved)
+
+    '''
     for index in toberemoved:
-        print(jManifest['items'][index-removed]['label']['italian'])
-        jManifest['items'].pop(index-removed)
+        # print(jManifest['items'][index-removed]['label']['italian'])
+        # jManifest['items'].pop(index-removed)
+        print("replaced_items:")
+        print(jManifest['items'][index]['items'][0]['items'])
         removed +=1
+    '''
+
 
 
     fout = open(manifest_dir+"/new/"+os.path.basename(manifest), "w")
     json.dump(jManifest,fout,indent=4)
     fout.close()
+
